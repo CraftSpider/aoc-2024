6b7b6f34e70e8d92efb4fd7525a1f6e2cd4e3526
CraftSpider <runetynan@gmail.com>
2024-12-03 09:35:57 -0800

    Day 3, Part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -4,19 +4,30 @@ use std::str::FromStr;
 
 advent_of_code::solution!(3);
 
-fn parser<'a>() -> Parser!['a, Vec<(u32, u32)>] {
+#[derive(Copy, Clone)]
+enum Instr {
+    Mul(u32, u32),
+    Do,
+    Dont,
+}
+
+fn parser<'a>() -> Parser!['a, Vec<Instr>] {
     let int = text::digits(10)
         .at_least(1)
         .at_most(3)
         .to_slice()
         .map(u32::from_str)
         .unwrapped();
-    let valid_instr = just("mul(")
+    let mul_instr = just("mul(")
         .ignore_then(int)
         .then_ignore(just(','))
         .then(int)
         .then_ignore(just(')'))
-        .map(Some);
+        .map(|(a, b)| Instr::Mul(a, b));
+    let do_instr = just("do()").to(Instr::Do);
+    let dont_instr = just("don't()").to(Instr::Dont);
+
+    let valid_instr = choice((mul_instr, do_instr, dont_instr)).map(Some);
 
     valid_instr
         .or(any().to(None))
@@ -26,12 +37,31 @@ fn parser<'a>() -> Parser!['a, Vec<(u32, u32)>] {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let out = parser().parse(input).unwrap();
-    Some(out.into_iter().map(|(a, b)| a * b).sum())
+    let vals = parser().parse(input).unwrap();
+    Some(
+        vals.into_iter()
+            .filter_map(|instr| {
+                if let Instr::Mul(a, b) = instr {
+                    Some(a * b)
+                } else {
+                    None
+                }
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let vals = parser().parse(input).unwrap();
+    let out = vals
+        .into_iter()
+        .fold((0, true), |(acc, do_mul), instr| match instr {
+            Instr::Mul(a, b) => (acc + (a * b * do_mul as u32), do_mul),
+            Instr::Do => (acc, true),
+            Instr::Dont => (acc, false),
+        })
+        .0;
+    Some(out)
 }
 
 #[cfg(test)]
@@ -47,6 +77,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(48));
     }
 }
