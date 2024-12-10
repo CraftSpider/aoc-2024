c14ee34989ffce328b010cdc6b17595f92318feb
CraftSpider <runetynan@gmail.com>
2024-12-10 15:49:22 -0800

    Significantly faster Day 9, Part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -7,28 +7,26 @@ advent_of_code::solution!(9);
 struct Disk {
     data: Vec<Option<u64>>,
     bytes: usize,
-    files: u64,
 }
 
 impl Disk {
     fn from_input(input: &str) -> Disk {
-        let (data, bytes, files, _) =
+        let (data, bytes, _) =
             input
                 .trim()
                 .chars()
                 .enumerate()
-                .fold((Vec::new(), 0, 0, true), |mut acc, (idx, c)| {
+                .fold((Vec::new(), 0, true), |mut acc, (idx, c)| {
                     let num = (c as u8 - b'0') as usize;
-                    if acc.3 {
+                    if acc.2 {
                         acc.0.extend(iter::repeat_n(Some(idx as u64 / 2), num));
                         acc.1 += num;
-                        acc.2 += 1;
                     } else {
                         acc.0.extend(iter::repeat_n(None, num))
                     }
-                    (acc.0, acc.1, acc.2, !acc.3)
+                    (acc.0, acc.1, !acc.2)
                 });
-        Disk { data, bytes, files }
+        Disk { data, bytes }
     }
 
     fn compress_bytes(&mut self) {
@@ -41,7 +39,7 @@ impl Disk {
         });
     }
 
-    fn iter_empty_range(&self) -> impl Iterator<Item = Range<usize>> + '_ {
+    fn empty_ranges(&self) -> impl Iterator<Item = Range<usize>> + '_ {
         let mut iter = self.data.iter();
         let mut idx = 0;
         from_fn(move || {
@@ -50,43 +48,59 @@ impl Disk {
             }
             let start = idx;
             idx += 1;
-
-            loop {
-                match iter.next() {
-                    Some(None) => idx += 1,
-                    Some(Some(_)) => {
-                        idx += 1;
-                        break;
-                    }
-                    None => {
-                        idx += 1;
-                        break;
-                    }
-                }
+            while let Some(None) = iter.next() {
+                idx += 1;
             }
-            Some(Range {
-                start,
-                end: idx - 1,
-            })
+            let end = idx;
+            idx += 1;
+            Some(Range { start, end })
         })
     }
 
-    fn file(&self, id: u64) -> Range<usize> {
-        let start = self.data.iter().position(|&val| val == Some(id)).unwrap();
-        let end = self.data.iter().rposition(|&val| val == Some(id)).unwrap();
-        start..end + 1
+    fn file_ranges(&self) -> impl Iterator<Item = (u64, Range<usize>)> + '_ {
+        let mut file_idx = 0;
+        let mut idx = 0;
+        from_fn(move || {
+            file_idx += 1;
+
+            while let Some(None) = self.data.get(idx) {
+                idx += 1;
+            }
+            let fidx = self.data.get(idx)?.unwrap();
+            let start = idx;
+            loop {
+                if self.data.get(idx).copied() == Some(Some(fidx)) {
+                    idx += 1;
+                } else {
+                    break;
+                }
+            }
+            let end = idx;
+            if let Some(None) | None = self.data.get(idx) {
+                idx += 1;
+            }
+            Some((file_idx - 1, Range { start, end }))
+        })
     }
 
-    fn try_compact_file(&mut self, idx: u64) {
-        let file = self.file(idx);
-        let Some(slot) = self
-            .iter_empty_range()
+    fn compress_files(&mut self) {
+        let files = self.file_ranges().collect::<Vec<_>>();
+        let mut empties = self.empty_ranges().collect::<Vec<_>>();
+        for file in files.into_iter().rev() {
+            self.try_compact_file(file.0, file.1, &mut empties);
+        }
+    }
+
+    fn try_compact_file(&mut self, idx: u64, file: Range<usize>, ranges: &mut [Range<usize>]) {
+        let Some(slot) = ranges
+            .iter_mut()
             .find(|r| file.len() <= r.len() && file.start > r.start)
         else {
             return;
         };
         self.data[file.clone()].fill(None);
         self.data[slot.start..slot.start + file.len()].fill(Some(idx));
+        slot.start = slot.start + file.len();
     }
 
     fn checksum(&self) -> u64 {
@@ -149,9 +163,7 @@ pub fn part_one(input: &str) -> Option<u64> {
 pub fn part_two(input: &str) -> Option<u64> {
     let mut disk = Disk::from_input(input);
     // disk.print();
-    for idx in (0..disk.files).rev() {
-        disk.try_compact_file(idx);
-    }
+    disk.compress_files();
     // disk.print();
     Some(disk.checksum())
 }
@@ -163,25 +175,45 @@ mod tests {
     #[test]
     fn test_iter_empty() {
         let disk = Disk::from_input("1234");
-        assert_eq!(
-            disk.iter_empty_range().collect::<Vec<_>>(),
-            vec![1..3, 6..10]
-        );
+        assert_eq!(disk.empty_ranges().collect::<Vec<_>>(), vec![1..3, 6..10]);
 
         let disk = Disk::from_input("0120");
-        assert_eq!(disk.iter_empty_range().collect::<Vec<_>>(), vec![0..1]);
+        assert_eq!(disk.empty_ranges().collect::<Vec<_>>(), vec![0..1]);
 
         let disk = Disk::from_input("2333133121414131402");
         assert_eq!(
-            disk.iter_empty_range().collect::<Vec<_>>(),
+            disk.empty_ranges().collect::<Vec<_>>(),
             vec![2..5, 8..11, 12..15, 18..19, 21..22, 26..27, 31..32, 35..36]
         )
     }
 
     #[test]
-    fn test_file_range() {
+    fn test_iter_files() {
         let disk = Disk::from_input("1234");
-        assert_eq!(disk.file(1), 3..6)
+        assert_eq!(
+            disk.file_ranges().collect::<Vec<_>>(),
+            vec![(0, 0..1), (1, 3..6)]
+        );
+
+        let disk = Disk::from_input("0120");
+        assert_eq!(disk.file_ranges().collect::<Vec<_>>(), vec![(0, 1..3)]);
+
+        let disk = Disk::from_input("2333133121414131402");
+        assert_eq!(
+            disk.file_ranges().collect::<Vec<_>>(),
+            vec![
+                (0, 0..2),
+                (1, 5..8),
+                (2, 11..12),
+                (3, 15..18),
+                (4, 19..21),
+                (5, 22..26),
+                (6, 27..31),
+                (7, 32..35),
+                (8, 36..40),
+                (9, 40..42),
+            ]
+        )
     }
 
     #[test]
