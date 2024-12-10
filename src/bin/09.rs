a6cafa4718fae349158a3d4afc8b9125e17d198a
CraftSpider <runetynan@gmail.com>
2024-12-10 10:28:18 -0800

    Day 9, Part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,30 +1,35 @@
+use itertools::Itertools;
 use std::iter;
+use std::iter::from_fn;
+use std::ops::Range;
 
 advent_of_code::solution!(9);
 
 struct Disk {
     data: Vec<Option<u64>>,
     bytes: usize,
+    files: u64,
 }
 
 impl Disk {
     fn from_input(input: &str) -> Disk {
-        let (data, bytes, _) =
+        let (data, bytes, files, _) =
             input
                 .trim()
                 .chars()
                 .enumerate()
-                .fold((Vec::new(), 0, true), |mut acc, (idx, c)| {
+                .fold((Vec::new(), 0, 0, true), |mut acc, (idx, c)| {
                     let num = (c as u8 - b'0') as usize;
-                    if acc.2 {
+                    if acc.3 {
                         acc.0.extend(iter::repeat_n(Some(idx as u64 / 2), num));
                         acc.1 += num;
+                        acc.2 += 1;
                     } else {
                         acc.0.extend(iter::repeat_n(None, num))
                     }
-                    (acc.0, acc.1, !acc.2)
+                    (acc.0, acc.1, acc.2, !acc.3)
                 });
-        Disk { data, bytes }
+        Disk { data, bytes, files }
     }
 
     fn rightmost_full(&self) -> usize {
@@ -39,18 +44,58 @@ impl Disk {
         self.data[self.bytes..].iter().all(|&val| val.is_none())
     }
 
-    fn most_compact_files(&self) -> bool {
-        todo!()
-    }
-
     fn move_rightmost_byte(&mut self) {
         let byte = self.rightmost_full();
         let empty = self.leftmost_empty();
         self.data.swap(byte, empty);
     }
 
-    fn move_rightmost_file(&mut self) {
-        todo!()
+    fn iter_empty_range(&self) -> impl Iterator<Item = Range<usize>> + '_ {
+        let mut iter = self.data.iter();
+        let mut idx = 0;
+        from_fn(move || {
+            while let Some(_) = iter.next()? {
+                idx += 1;
+            }
+            let start = idx;
+            idx += 1;
+
+            loop {
+                match iter.next() {
+                    Some(None) => idx += 1,
+                    Some(Some(_)) => {
+                        idx += 1;
+                        break;
+                    }
+                    None => {
+                        idx += 1;
+                        break;
+                    }
+                }
+            }
+            Some(Range {
+                start,
+                end: idx - 1,
+            })
+        })
+    }
+
+    fn file(&self, id: u64) -> Range<usize> {
+        let start = self.data.iter().position(|&val| val == Some(id)).unwrap();
+        let end = self.data.iter().rposition(|&val| val == Some(id)).unwrap();
+        start..end + 1
+    }
+
+    fn try_compact_file(&mut self, idx: u64) {
+        let file = self.file(idx);
+        let Some(slot) = self
+            .iter_empty_range()
+            .find(|r| file.len() <= r.len() && file.start > r.start)
+        else {
+            return;
+        };
+        self.data[file.clone()].fill(None);
+        self.data[slot.start..slot.start + file.len()].fill(Some(idx));
     }
 
     fn checksum(&self) -> u64 {
@@ -115,8 +160,8 @@ pub fn part_one(input: &str) -> Option<u64> {
 pub fn part_two(input: &str) -> Option<u64> {
     let mut disk = Disk::from_input(input);
     // disk.print();
-    while !disk.most_compact_files() {
-        disk.move_rightmost_file();
+    for idx in (0..disk.files).rev() {
+        disk.try_compact_file(idx);
     }
     // disk.print();
     Some(disk.checksum())
@@ -125,6 +170,30 @@ pub fn part_two(input: &str) -> Option<u64> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_iter_empty() {
+        let disk = Disk::from_input("1234");
+        assert_eq!(
+            disk.iter_empty_range().collect::<Vec<_>>(),
+            vec![1..3, 6..10]
+        );
+
+        let disk = Disk::from_input("0120");
+        assert_eq!(disk.iter_empty_range().collect::<Vec<_>>(), vec![0..1]);
+
+        let disk = Disk::from_input("2333133121414131402");
+        assert_eq!(
+            disk.iter_empty_range().collect::<Vec<_>>(),
+            vec![2..5, 8..11, 12..15, 18..19, 21..22, 26..27, 31..32, 35..36]
+        )
+    }
+
+    #[test]
+    fn test_file_range() {
+        let disk = Disk::from_input("1234");
+        assert_eq!(disk.file(1), 3..6)
+    }
 
     #[test]
     fn test_part_one() {
